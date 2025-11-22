7a648dcdce502ed82c4b6d2a2b4625e61c216d96
Eva Frankovic <emfrankovic7@gmail.com>
2025-11-22 18:37:44 -0500

    NavBar - issue with dropdown background styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,30 +1,66 @@
 import {
     NavigationMenu,
     NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
-    NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import laprimaLogo from "@/public/images/LaPrima-logo-1.png";
 
-export default function Home() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home({ cartCount = 0 }: { cartCount?: number }) {
     return (
-        <div>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Link</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
+        <div className="w-full flex items-center justify-between px-10 py-4 bg-zinc-700">
+            <div>
+                <Image
+                    src={laprimaLogo}
+                    alt="La Prima Coffee Logo"
+                    width={170}
+                    height={170}
+                    className="cursor-pointer"
+                />
+            </div>
+
+            <div className="flex items-center gap-8">
+                <NavigationMenu>
+                    <NavigationMenuList className="flex items-center gap-6 text-white">
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent data-[state=open]:bg-transparent shadow-none">
+                                Shop
+                            </NavigationMenuTrigger>
+
+                            <NavigationMenuContent className="bg-transparent shadow-none border-none">
+                                <NavigationMenuLink className="bg-transparent hover:bg-transparent">Link</NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className="bg-transparent hover:bg-transparent hover:text-blue-600">About</NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className="bg-transparent hover:bg-transparent hover:text-blue-600">Locations</NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className="bg-transparent hover:bg-transparent hover:text-blue-600">Contact</NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+
+                <button
+                    type="button"
+                    className="relative inline-flex items-center text-white hover:text-blue-600"
+                    aria-label="Open shopping cart"
+                // Make cart clickable here, as well as implement counting of items?
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} className="h-6 w-6" />
+                    <span className="ml-1">{cartCount}</span>
+                </button>
+            </div>
         </div>
     );
 }
-
-
-
