3d8481c4bc8c150e31b9bf1eb63baa44464e708d
Eva Frankovic <emfrankovic7@gmail.com>
2025-11-22 21:41:55 -0500

    Removed dropdown, still need to do count

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,8 @@
 import {
     NavigationMenu,
-    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import Image from "next/image";
 import laprimaLogo from "@/public/images/LaPrima-logo-1.png";
@@ -14,29 +12,25 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({ cartCount = 0 }: { cartCount?: number }) {
     return (
-        <div className="w-full flex items-center justify-between px-10 py-4 bg-zinc-700">
+        <div className="w-full flex items-center justify-between px-30 py-4 bg-zinc-700">
             <div>
                 <Image
                     src={laprimaLogo}
                     alt="La Prima Coffee Logo"
-                    width={170}
-                    height={170}
+                    width={150}
+                    height={150}
                     className="cursor-pointer"
                 />
             </div>
 
-            <div className="flex items-center gap-8">
+            <div className="flex items-center gap-3">
                 <NavigationMenu>
                     <NavigationMenuList className="flex items-center gap-6 text-white">
+                    
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger className="bg-transparent hover:bg-transparent focus:bg-transparent data-[state=open]:bg-transparent shadow-none">
-                                Shop
-                            </NavigationMenuTrigger>
-
-                            <NavigationMenuContent className="bg-transparent shadow-none border-none">
-                                <NavigationMenuLink className="bg-transparent hover:bg-transparent">Link</NavigationMenuLink>
-                            </NavigationMenuContent>
+                            <NavigationMenuLink className="bg-transparent hover:bg-transparent hover:text-blue-600">Shop</NavigationMenuLink>
                         </NavigationMenuItem>
+
                         <NavigationMenuItem>
                             <NavigationMenuLink className="bg-transparent hover:bg-transparent hover:text-blue-600">About</NavigationMenuLink>
                         </NavigationMenuItem>
@@ -53,7 +47,7 @@ export default function Home({ cartCount = 0 }: { cartCount?: number }) {
 
                 <button
                     type="button"
-                    className="relative inline-flex items-center text-white hover:text-blue-600"
+                    className="relative inline-flex items-center text-white hover:text-blue-600 gap-3"
                     aria-label="Open shopping cart"
                 // Make cart clickable here, as well as implement counting of items?
                 >
